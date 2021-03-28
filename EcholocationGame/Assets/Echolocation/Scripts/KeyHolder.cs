82a073a9d0f48023957623b9ca94737ee47fd00f
Perry <57232543+Perry1903@users.noreply.github.com>
2021-03-28 02:00:19 +0100

    Added key and door interaction

---
+++ EcholocationGame/Assets/Echolocation/Scripts/KeyHolder.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyHolder : MonoBehaviour
+{
+    private List<Key> keys;
+
+    private void Awake()
+    {
+        keys = new List<Key>();
+    }
+
+    public void AddKey(Key key)
+    {
+        Debug.Log("Picked up a key");
+        keys.Add(key);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Key key = collision.GetComponent<Key>();
+        if (key != null)
+        {
+            AddKey(key);
+            Destroy(key.gameObject);
+        }
+
+        Door door = collision.GetComponent<Door>();
+        if (door != null)
+        {
+            door.OpenDoor();
+        }
+    }
+}
